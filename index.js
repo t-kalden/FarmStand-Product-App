17b952973b68c6d2368510d0207182ab9ea5fab8
t-kalden <kalden.t@outlook.com>
2022-02-01 22:24:51 -0500

    Utilizing mongoose to connect to mongoDB, node.js, and Express and ejs. The application creates a database using MongoDB to store products name, price and category.

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand') 
+.then(() => {
+    console.log("MONGO CONNECTION OPEN");
+})
+.catch(err => {
+    console.log("MONGO CONNECTION ERROR");
+    console.log(err);
+})
+
+app.set('views', path.join(__dirname), 'views');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+///route to display all products from mongoDB 
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('views/products/index.ejs', {products});
+})
+
+//route to render form to add new product to mongoDB
+app.get('/products/new', (req,res) => {
+    res.render('views/products/new.ejs');
+})
+
+//adding new product to mongoDB
+app.post('/products', async (req,res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+})
+
+//find product by id from mongoDB
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('views/products/show.ejs', {product});
+})
+
+//find and edit product  by id in mongo db
+app.get('/products/:id/edit', async (req, res) =>{
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('views/products/edit.ejs', {product});
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async(req,res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect(`/products`);
+})
+app.listen(3000, ()=> {
+    console.log("APP IS LISTENING ON PORT 3000");
+}) 
